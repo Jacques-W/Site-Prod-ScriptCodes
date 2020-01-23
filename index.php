ce3dd6d3c7bb9af8b0977f3491162d1c59c3c0be
Jacques-W <jacques.developpement.web@gmail.com>
2020-01-23 15:49:45 +0100

    Mise à Jour du CSS nettoyage de code page equipe.php plus ajout lien fb et twitter.

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<!-- Ajouter le css Personnalisé ICI ligne 6->faire un retour à la ligne -->
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+		<title>ScriptCodes</title>
+	</head>
+	<!-- header avec Barre de Nav -->
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<a class="navbar-brand" href="index.php">ScriptCodes</a>
+    			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#barreNav" aria-expanded="false">
+    				<span class="navbar-toggler-icon"></span>
+    			</button>
+			<div class="collapse navbar-collapse" id="barreNav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php">Accueil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="services.php">Nos Services</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="productions.php">Nos productions</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="propos.php">A propos de Nous</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="equipe.php">Notre &eacute;quipe</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+</html>
