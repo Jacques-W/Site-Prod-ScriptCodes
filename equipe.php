a83e8071482c1637687a520cffc24962f196c9ed
Jacques-W <60091045+Jacques-W@users.noreply.github.com>
2020-01-24 15:23:34 +0100

    Add files via upload
    Mise à jour 24 01 2020 15h22, todo en cours de test applicatif

--- equipe.php
+++ equipe.php
@@ -11,7 +11,7 @@
 		<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 		<title>ScriptCodes</title>
 	</head>
-	<body id="body">
+	<body>
     	<header>
     		<nav class="navbar navbar-expand-lg navbar-dark">
     			<a class="navbar-brand" href="index.php">ScriptCodes</a>
@@ -32,8 +32,8 @@
     					<li class="nav-item">
     						<a class="nav-link disabled" href="propos.php">A propos de Nous</a>
     					</li>
-    					<li class="nav-item">
-    						<a class="nav-link disabled" href="equipe.php">Notre &eacute;quipe</a>
+    					<li class="nav-item active">
+    						<a class="nav-link" href="equipe.php">Notre &eacute;quipe</a>
     					</li>
     				</ul>
     			</div>
@@ -127,15 +127,21 @@
     			</div>
     		</section>
     	</section>
-    	<footer id="footer">
-    		<section class="container">
+    	<footer>
+    		<section class="container text-center">
     			<div class="row">
     				<div class="col-md-12 text-center text-light">
-    					<p>©️copyright 2020 - Tous droits r&eacute;serv&eacute;s &agrave; ScriptCodes - Propuls&eacute; par ScriptCodes</p>
+    					<p>© copyright 2020 - Tous droits r&eacute;serv&eacute;s &agrave; ScriptCodes - Propuls&eacute; par ScriptCodes</p>
     				</div>
-    				<div class="col-md-4 text-light">
-    					<p><span style="font-size: 1.5em; color: Dodgerblue;"><i class="fab fa-facebook"></i></span><a href="https://facebook.com/ScriCodes-106267850925400/" target="_blank">ScriptCodes</a>
+    				<div class="col-md-4">
+                        <p><span id="logoFaceBook"><i class="fab fa-facebook"></i></span><a href="https://facebook.com/ScriCodes-106267850925400/" target="_blank"> ScriptCodes</a></p>
     				</div>
+                    <div class="col-md-4">
+                        <p><span id="logoTwitter"><i class="fab fa-twitter"></i></span><a href="https:/twitter.com/ScriCodes" target="_blank"> ScriptCodes</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <p><span id="logoLinkedIn"><i class="fab fa-linkedin-in"></i></span><a href="https://www.linkedin.com/in/jacques-wickersheim-69b138192" target="_blank"> ScriptCodes</a></p>
+                    </div>
     			</div>
     		</section>
     	</footer>
--- index.php
+++ index.php
@@ -1,40 +1,62 @@
 <!DOCTYPE html>
 <html>
-	<!-- Ajouter le css Personnalis� ICI ligne 6->faire un retour � la ligne -->
+	<!-- Ajouter le css Personnalisé ICI ligne 6->faire un retour à la ligne -->
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="css/main.css">
+        <script src="https://kit.fontawesome.com/e6dde792b7.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 		<title>ScriptCodes</title>
 	</head>
-	<!-- header avec Barre de Nav -->
-	<header>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-			<a class="navbar-brand" href="index.php">ScriptCodes</a>
-    			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#barreNav" aria-expanded="false">
-    				<span class="navbar-toggler-icon"></span>
-    			</button>
-			<div class="collapse navbar-collapse" id="barreNav">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="index.php">Accueil</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="services.php">Nos Services</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="productions.php">Nos productions</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="propos.php">A propos de Nous</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="equipe.php">Notre &eacute;quipe</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	</header>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <a class="navbar-brand" href="index.php">ScriptCodes</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#barreNav" aria-expanded="false">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="barreNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="servives.php">Nos Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="productions.php">Nos Productions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="propos.php">A propos de Nous</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="equipe.php">Notre équipe</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- ADD LE CONTENU -->
+        <footer>
+            <section class="container text-center">
+                <div class="row">
+                    <div class="col-md-12 text-center text-light">
+                        <p>© copyright 2020 - Tous droits réservés à ScriptCodes - Propulsé par ScriptCodes</p>
+                    </div>
+                    <div class="col-md-4">
+                        <p><span id="logoFaceBook"><i class="fab fa-facebook"></i></span><a href="https://facebook.com/SciCodes-106267850925400/" target="_blank"> ScriptCodes</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <p><span id="logoTwitter"><i class="fab fa-twitter"></i></span><a href="https://twitter.com/ScriCodes" target="_blank"> ScriptCodes</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <p><span id="logoLinkedIn"><i class="fab fa-linkedin-in"></i></span><a href="https://www.linkedin.com/in/jacques-wickersheim-69b13192" target="_blank"> ScriptCodes</a></p>
+                    </div>
+                </div>
+            </section>
+        </footer>
+    </body>
 </html>
