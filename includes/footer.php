c022148d987ce5b7a53944908671cc56c1480d0b
Nathaelle Dorval <nathaelle.dorval@hotmail.fr>
2020-01-25 16:45:15 +0100

    footer

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<footer>
+    <section class="container text-center py-3">
+        <div class="row">
+            <div class="col-md-4 text-center text-light">
+                <p>Contact</p>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 text-center text-light">
+                <p>Mentions légales</p>
+            </div>
+        <div class="col-4">
+        <div class="row">
+            <div class="col-md-4">
+                <p><span id="logoFaceBook" class="text-light"><i class="fab fa-facebook"></i></span><a href="https://facebook.com/SciCodes-106267850925400/" target="_blank"> ScriptCodes</a></p>
+            </div>
+            <div class="col-md-4">
+                <p><span id="logoTwitter" class="text-light"><i class="fab fa-twitter"></i></span><a href="https://twitter.com/ScriCodes" target="_blank"> ScriptCodes</a></p>
+            </div>
+            <div class="col-md-4">
+                <p><span id="logoLinkedIn" class="text-light"><i class="fab fa-linkedin-in"></i></span><a href="https://www.linkedin.com/in/jacques-wickersheim-69b13192" target="_blank"> ScriptCodes</a></p>
+            </div>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+        </div>
+        <div class="col-md-12 text-center text-light">
+                <p>© Copyright 2020 - Tous droits réservés à ScriptCodes - Propulsé par ScriptCodes</p>
+            </div>
+        </div>
+    </section>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="index.php">ScriptCodes</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#barreNav" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="barreNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="services.php">Nos Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="productions.php">Nos Productions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="propos.php">A Propos de Nous</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="equipe.php">Notre Equipe</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
