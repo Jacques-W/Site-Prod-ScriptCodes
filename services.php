7e21811521bc6f365926520ccd44a55d5761f0ad
abiespectinata <60095793+abiespectinata@users.noreply.github.com>
2020-01-26 21:54:42 +0100

    Add files via upload

--- services.php
+++ services.php
@@ -1,44 +1,9 @@
 <!DOCTYPE html>
 <html>
-<?php include 'includes/functions.php'; ?>
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
-    <script src="https://kit.fontawesome.com/e6dde792b7.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E25Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <title>ScriptCodes</title>
-</head>
-<body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="index.php">ScriptCodes</a>
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#barreNav" aria-expanded="false">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="barreNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="https://scricodes.com">Accueil</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="services.php">Nos Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="productions.php">Nos Productions</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="propos.php">A Propos de Nous</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="equipe.php">Notre Equipe</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<?php include 'includes/functions.php';
+include 'includes/head.php'; 
+include 'includes/header.php';  ?>
+
 <section class="container-fluid text-center text-light">
     <section class="container">
         <div class="row">
